8bd297f1138f2700548f6e2e929c4cf9167ec562
Guilherme Henrique <guilherme78692@gmail.com>
2025-09-28 19:24:54 -0300

    Projeto Spring

---
+++ Projeto Spring inicial/Projeto-Spring-inicial/src/main/java/com/example/Projeto/Spring/inicial/ProjetoSpringInicialApplication.java
@@ -0,0 +1,13 @@
+package com.example.Projeto.Spring.inicial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringInicialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringInicialApplication.class, args);
+	}
+
+}
